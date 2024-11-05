34f87c4c740a0a1619e711fb05b0ec2255566d07
ARUN YERRAM <arunyerram12022005@gmail.com>
2024-11-05 23:45:52 +0530

    Practice

---
+++ .history/GRAPH/Prims_20241105234545.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
+        vector<int>vis(V,0);
+        
+        pq.push({0,0});
+        
+        int sum = 0;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            
+            int node = it.second;
+            int value = it.first;
+            
+            if(vis[node] == 1)continue;
+            
+            vis[node] = 1;
+            
+            sum += value;
+            
+            for(auto it:adj[node]){
+                int adjnode = it[0];
+                int edW = it[1];
+                if(!vis[adjnode]){
+                    pq.push({edW,adjnode});
+                }
+            }
+        }
+        return sum;
+    }
+    
